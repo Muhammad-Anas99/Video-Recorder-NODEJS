04d19a0bcc2df9deb8cc63d08f7ea01912cfb2d9
MuhammadAnas <flashycoderch@gmail.com>
2024-11-17 10:01:14 +0500

    a

--- public/script.js
+++ public/script.js
@@ -1,10 +1,26 @@
 const video = document.getElementById("user-video");
-const start = document.getElementById("start-btn");
+const startButton = document.getElementById("start-btn");
+
+const state = { media: null };
+
+startButton.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    videoBitsPerSecond: 12800,
+    audioBitsPerSecond: 2500000,
+    framerate: 50,
+  });
+
+  mediaRecorder.ondataavailable = (e) => {
+    console.log("Binary Stream Available", e.data);
+  };
+  mediaRecorder.start(25);
+});
 
 window.addEventListener("load", async (e) => {
   const media = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  state.media = media;
   video.srcObject = media;
 });
